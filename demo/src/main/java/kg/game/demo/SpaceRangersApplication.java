3c920cd12a3382ea5daa0fe74f2b208b4c353c24
Alexander <89515014507@mail.ru>
2021-10-17 04:00:01 +0600

    Start game project

---
+++ demo/src/main/java/kg/game/demo/SpaceRangersApplication.java
@@ -0,0 +1,13 @@
+package kg.game.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceRangersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceRangersApplication.class, args);
+	}
+
+}
