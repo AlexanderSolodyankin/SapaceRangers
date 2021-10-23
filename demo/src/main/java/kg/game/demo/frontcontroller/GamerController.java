f22f37994af7357f236101b24f06ef2b34d2186b
Alexander <89515014507@mail.ru>
2021-10-24 00:21:09 +0600

    с пост запросами закончил можно приступать к созданию логики игры

--- demo/src/main/java/kg/game/demo/frontcontroller/GamerController.java
+++ demo/src/main/java/kg/game/demo/frontcontroller/GamerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("/gamer")
 public class GamerController{
@@ -18,5 +20,15 @@ public class GamerController{
         Iterable<Gamer> gamers = gamerRepository.findAll();
         model.addAttribute("gamers", gamers);
         return "gamer";
+    }
+    @GetMapping("/gamer/newGamer")
+    public String newGamer(Model model){
+        return "newGamer";
+    }
+    @PostMapping("/gamer/newGamer")
+    public String saveGamer(@RequestParam String fullName,  Model model){
+        Gamer gamer = new Gamer().builder().fullName(fullName).build();
+        gamerRepository.save(gamer);
+        return "redirect:/gamer";
     }
 }
