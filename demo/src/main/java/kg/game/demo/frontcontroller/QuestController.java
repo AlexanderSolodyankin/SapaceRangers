c04a0e866c5855d72da0f3b937525313446329ae
Alexander <89515014507@mail.ru>
2021-10-29 13:45:39 +0600

    реалезована кнопка переноса данных (подготовка к реалезации квеста)

--- demo/src/main/java/kg/game/demo/frontcontroller/QuestController.java
+++ demo/src/main/java/kg/game/demo/frontcontroller/QuestController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +26,10 @@ public class QuestController {
 
         model.addAttribute("quests", quests);
         return "quests";
+    }
+    @GetMapping("/quests/quest1")
+    public String questGet(@RequestParam String questName, Model model){
+        System.out.println("Я получил " + questName);
+        return "quest1";
     }
 }
