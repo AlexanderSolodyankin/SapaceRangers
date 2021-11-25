eb88fde232dccbe4e6e70384454fb48eae3668ac
AlexanderSolodyankin <89515014507@mail.ru>
2021-11-25 20:44:20 +0600

    Добавил медику как шпаргалку

--- demo/src/main/java/draft/draft.java
+++ demo/src/main/java/draft/draft.java
@@ -1,18 +1,36 @@
 package draft;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class draft {
     public static void main(String[] args) {
-//        List<String>  right1 = Arrays.asList(new String[]{"Чук", "Ахилес", "Балл", "Грогу"});
+
+//        int age = 90;
+//        String name = "петя";
+//        char ch1 = 'л';
+//        char ch2 = 'е';
+//        char ch3 = 'т';
+//        int num = 57;
 //
-//        right1.
-//        System.out.println(right1);
+//        age = num - age;
+//
+//        System.out.println(name + " " + age + " " + ch1 + ch2 + ch3);
+        int[] num23 = {1,2,3};
 
 
+        try {
+
+            System.out.println(num23[20]);
+
+        }
+        catch (Exception e) {
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+
+        System.err.println("Азат смотри завершение кода");
 
     }
 }
